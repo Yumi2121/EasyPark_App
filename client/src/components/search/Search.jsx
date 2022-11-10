fd42e59d4b013c6f16e802d3f25be911ac69cac0
Yumi2121 <zumin2016@gmail.com>
2022-11-10 12:55:49 +1100

    styleing the search page

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -4,14 +4,21 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import "./search.css"
 
 // URL constructor
-// const picture = new URL("./images/bear.jpeg", import.meta.url)
+const whitePicture = new URL("../../images/white-bg2.jpg", import.meta.url)
 
 const Search = () => {
     return (
          <div className="search-session">
             <div className="search-details">
+                <p><h1>New Option of Parking- EasyPark</h1></p>
+                <p>Book now! Pay later </p>
+                <p>Where do you want to Park?  </p> 
                 <FontAwesomeIcon icon={faMagnifyingGlass} />
-                <input type="text" placeholder="Where are you going?" classname="searchInput" />
+                <input type="text" placeholder="Search for the address" classname="searchInput" />
+            </div>
+
+            <div className="white-bg-container" >
+                <img className="white-bg-image" src={whitePicture} />
             </div>
         </div>
     )
