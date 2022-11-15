6b65b7db44d5bfccbb7b911f8665903fda27bde3
Wokko-wok <philliplam9@gmail.com>
2022-11-15 14:47:36 +1000

    removed spelling mistake

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -24,7 +24,7 @@ export const useSignup = () => {
     if (response.ok) {
       // save the user to local storage
       localStorage.setItem('user', JSON.stringify(json))
-git
+      
       // update the auth context
       dispatch({type: 'LOGIN', payload: json})
 
