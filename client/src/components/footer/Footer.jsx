3416e87b43eca62386a78a4fa2f7c1c2578ab7b7
Wokko-wok <philliplam9@gmail.com>
2022-11-17 08:49:57 +1000

    Added Footer and some About elements

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+        <Container>
+            <Row>
+                <Col>&copy; EasyPark 2022</Col>
+            </Row>
+        </Container>
+    </footer>
+  )
+}
+export default Footer
