98fcb5ed99d67085f0086562f7cd4ba1fec51cf0
Wokko-wok <philliplam9@gmail.com>
2022-11-17 14:09:42 +1000

    finished about

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -3,25 +3,42 @@ import {Container, Row, Col} from 'react-bootstrap';
 import SafeIcon from "../../images/safe.png";
 import SaveIcon from "../../images/save.png";
 import FlexIcon from "../../images/flex.png";
+import "./About.css";
 
 const About = () => {
 return(
     <body>
         <Container>
-         <h1>
-            <p>About Us</p>
-         </h1>
+          <div className="about-title">
+            <h1>About Us</h1>
+          </div>
          <Row>
           <Col>
-            <h3>
+            <h3 className="about-text">
                 EasyPark is an alternative to general parking that allows you to book a 
                 private parking spots in Melbourne, Australia with ease. 
             </h3>
           </Col>
         </Row>
-        <img src={SafeIcon} alt="Safe" width="200"/>
-        <img src={SaveIcon} alt="Savings" width="200"/>
-        <img src={FlexIcon} alt="Flexable" width="200"/>
+        <div className="icons">
+          <div class="about-subtitle">
+            <p>EasyPark Parking Spots are:</p>
+          </div>
+          <div class="flex-container">
+            <div class="container">
+              <p>Reliable & Secure</p>
+              <img src={SafeIcon} alt="Safe" width="200"/>
+            </div>
+            <div class="container">
+              <p>Easy To Find</p>
+              <img src={FlexIcon} alt="Flexable" width="200"/>
+            </div>
+            <div class="container">
+              <p>& Afordable!</p>
+              <img src={SaveIcon} alt="Savings" width="200"/>
+            </div>
+          </div>
+        </div>
         </Container>
     </body>
 
