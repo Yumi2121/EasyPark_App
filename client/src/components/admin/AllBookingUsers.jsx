c055103c58ff9a3c22c9d92713730c3d167a1544
Wokko-wok <philliplam9@gmail.com>
2022-11-25 13:21:42 +1000

    Added ability to see all users with some info about them as well as to see all bookings with users attached to them

--- client/src/components/admin/AllBookingUsers.jsx
+++ client/src/components/admin/AllBookingUsers.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import useFetch from '../../utils/useFetch';
+import moment from 'moment'
 
 
 const AllBookingUsers = () => {
-    const {data}  = useFetch("/bookings")
-    console.log(data)
+    const {data: bookings}  = useFetch("/bookings")
+    console.log(bookings)
+ 
 
-    return data.map((bookings, i) => (
+    const {data: users} = useFetch("/users")
+    console.log(users)
+
+    function getUserEmail(id) {
+      return users.find(user => user._id === id)?.email
+    }
+
+    if(!bookings && !users) return <></>
+
+    return bookings.map((booking, i) => (
       <div key={i} className="bookingdetails_display">
-        <h3>{bookings.carpark_name}</h3>
-        <p>{bookings.user}</p>
+        <h4>{booking.carpark_name}</h4>
+        <p>User: {getUserEmail(booking.user)}</p>
+        <p>Duration: {moment(booking.start_booking_date).format("DD/MM/YYYY")} to {moment(booking.end_booking_date).format("DD/MM/YYYY")}</p>
+
       </div>
     ));
   };
--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -3,6 +3,8 @@ import Sidebar from "../../components/sidebar/Sidebar";
 // import Admin from "../../components/admin/Admin";
 import "./Admin.css";
 import AllBookingUsers from "../../components/admin/AllBookingUsers";
+import AllUsers from "../../components/admin/AllUsers";
+
 
 
 
@@ -12,7 +14,12 @@ const Home = () => {
         <div>
             {/* <Sidebar />  */}
             <div>
-                <AllBookingUsers />          
+                <h2>This is all Bookings with Users</h2>
+                <AllBookingUsers /> 
+            </div>
+            <div>
+                <h2>This is all Users</h2>
+                <AllUsers />   
             </div>
         </div>
     )
