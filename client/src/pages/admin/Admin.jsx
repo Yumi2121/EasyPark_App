14c9a6ed4935399aea5b71e5b9d95486126130c3
Wokko-wok <philliplam9@gmail.com>
2022-11-25 15:49:32 +1000

    Added Side NavBar and also made top NavBar stick to the top of the page

--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -2,24 +2,14 @@ import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 // import Admin from "../../components/admin/Admin";
 import "./Admin.css";
-import AllBookingUsers from "../../components/admin/AllBookingUsers";
-import AllUsers from "../../components/admin/AllUsers";
-
-
-
-
+import {Outlet} from "react-router-dom";
 
 const Home = () => {
     return (
-        <div>
-            {/* <Sidebar />  */}
-            <div>
-                <h2>This is all Bookings with Users</h2>
-                <AllBookingUsers /> 
-            </div>
-            <div>
-                <h2>This is all Users</h2>
-                <AllUsers />   
+        <div style={{display: 'flex'}}>
+            <Sidebar />
+            <div style={{marginLeft: '270px'}}>
+                <Outlet />
             </div>
         </div>
     )
