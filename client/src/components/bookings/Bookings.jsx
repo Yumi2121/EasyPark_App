931db6ab27f31f9e499e1ce54c0dcaa49bf37908
Wokko-wok <philliplam9@gmail.com>
2022-11-27 21:11:20 +1000

    fixed user bookings page

--- client/src/components/bookings/Bookings.jsx
+++ client/src/components/bookings/Bookings.jsx
@@ -1,26 +1,37 @@
-import React from "react";
+import React from "react"
+import useFetch from '../../utils/useFetch'
+import moment from 'moment'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 import { useContext } from "react";
 import { AuthContext } from "../../utils/AuthContext";
-import { useState } from "react";
-import useFetch from '../../utils/useFetch';
-import Container from 'react-bootstrap/Container';
 
+
+import easyparkAPI from '../../config/api'
 
 const Bookings = () => {
-    const {data, loading, error, reFetch }  = useFetch("/api/bookings")
-    console.log(data)
 
-    //filter data by user:id & authcontect
+  const {user} = useContext(AuthContext);
+  const CurrentUserId = user.data._id;
+  const {data: bookings, reFetch: reFetchBookings}  = useFetch(`/bookings/user/${CurrentUserId}`)
+  console.log(bookings)
 
-    const bookingdetails = data
 
-    return bookingdetails.map((bookings, i) => (
-      <div key={i} className="bookingdetails_display">
-        <h3>{bookings.carpark_name}</h3>
-        <p>{bookings.start_booking_date}</p>
-        <p>{bookings.end_booking_date}</p>
-      </div>
-    ));
-  };
+  const handleClick = (bookingId) => {
+    easyparkAPI.delete(`/bookings/${bookingId}`)
+    .then(() => reFetchBookings())
+    .catch(() => console.log('oops something went wrong'))
+  }
+
+  return bookings.map((booking, i) => (
+    <Card style={{ height: '13rem', width: '40rem'}}>
+      <Card.Body key={i} className="bookingdetails_display">
+        <Card.Title>{booking.carpark_name}</Card.Title>
+        <Card.Text>Duration: {moment(booking.start_booking_date).format("DD/MM/YYYY")} to {moment(booking.end_booking_date).format("DD/MM/YYYY")}</Card.Text>
+        <Button onClick={() => handleClick(booking._id)} variant="primary">Delete</Button>
+      </Card.Body>
+    </Card>
+  ))
+}
 
 export default Bookings;
